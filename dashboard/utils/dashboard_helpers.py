4fd2ff88400f79ab7d13e02bd95caaa2f091fdae
Austin Rittenhouse <raustinj39@gmail.com>
2025-06-08 15:14:20 -0500

    Fix auto_refresh for Streamlit 1.25+

--- dashboard/utils/dashboard_helpers.py
+++ dashboard/utils/dashboard_helpers.py
@@ -25,4 +25,7 @@ def auto_refresh(interval: int = 5):
         st.session_state["_last_refresh"] = time.time()
     elif time.time() - st.session_state["_last_refresh"] > interval:
         st.session_state["_last_refresh"] = time.time()
-        st.experimental_rerun()
+        if hasattr(st, "rerun"):
+            st.rerun()
+        else:
+            st.experimental_rerun()
